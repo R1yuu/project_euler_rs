85b0a1d090319d59fa29bdc321a3a810c96f319e
R1yuu <andre.schneider@outlook.at>
2022-02-21 00:32:36 +0100

    Adds Readme and problem 3 and 4

---
+++ README.md
@@ -0,0 +1,3 @@
+<https://projecteuler.net>
+
+This is a project purely to teach myself rust.
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 // Multiples of 3 or 5
-fn problem_1() {
+fn problem_1(ceil: u32) {
     let mut sum: u32 = 0;
-    for x in (3..1000).step_by(3) {
+    for x in (3..ceil).step_by(3) {
         sum += x;
     }
-    for y in (5..1000).step_by(5) {
+    for y in (5..ceil).step_by(5) {
         sum += if y % 15 == 0 { 0 } else { y };
     }
 
@@ -12,12 +12,12 @@ fn problem_1() {
 }
 
 // Even Fibonacci numbers
-fn problem_2() {
+fn problem_2(ceil: u32) {
     let mut x:      u32 = 1;
     let mut y:      u32 = 2;
     let mut tmp:    u32;
     let mut sum = 0;
-    while x < 4000000 && y < 4000000 {
+    while x < ceil && y < ceil {
         if y % 2 == 0 {
             sum += y;
         }
@@ -29,7 +29,61 @@ fn problem_2() {
     println!("{}", sum);
 }
 
+// Largest prime factor
+fn problem_3(mut num: u64) {
+    let mut checked_primes = Vec::new();
+    let mut primes = Vec::new();
+    let mut curr_prime = 2;
+
+    while curr_prime <= num {
+        if num % curr_prime == 0 {
+            primes.push(curr_prime);
+            num /= curr_prime;
+        } else {
+            let mut is_not_prime;
+            checked_primes.push(curr_prime);
+
+            while {
+                is_not_prime = false;
+                curr_prime += 1;
+                for prime in &checked_primes {
+                    if curr_prime % prime == 0 {
+                        is_not_prime = true;
+                        break;
+                    }
+                }
+                is_not_prime
+            } {}
+        }
+    }
+
+    let max_prime = primes.iter().max();
+    match max_prime {
+        Some(max)   => println!("{}", max),
+        None        => println!("No Solution.")
+    }
+}
+
+// Largest palindrome product
+fn problem_4() {
+    let mut largest_palindrome = 0;
+    for i in 100..1000 {
+        for j in i..1000 {
+            let num = i * j;
+            let num_string = num.to_string();
+            let reversed_string = num_string.chars().rev().collect::<String>();
+            if num_string.eq(&reversed_string) && num > largest_palindrome {
+                largest_palindrome = num;
+            }
+        }
+    }
+
+    println!("{}", largest_palindrome);
+}
+
 fn main() {
-    problem_1();
-    problem_2();
+    problem_1(1000);
+    problem_2(4000000);
+    problem_3(600851475143);
+    problem_4();
 }
